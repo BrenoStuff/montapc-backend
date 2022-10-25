076b1dd88c1bcdfd6b4e9e05d607687b4bacbb13
Breno Machado <brenomachado0520@gmail.com>
2022-10-25 15:49:15 -0300

    Added basic files to start codding

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// PATHS and FOLDERS
+define('BASE_PATH','/PJ3/montapc-backend/');
+define('CONTROLLERS_FOLDER','controllers/');
+define('HELPERS_FOLDER','helpers/');
+define('MODELS_FOLDER','models/');
+
+// DATABASE
+define('DB_HOST','localhost');
+define('DB_NAME','montapc');
+define('DB_USER','root');
+define('DB_PASS','');
+
+// CORS
+define('ALLOWED_HOSTS','http://localhost:3000');
+?>
